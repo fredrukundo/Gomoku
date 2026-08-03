35df017ec9dc32ca19ac841d38751695936909ba
fredrukundo <dukefred9@gmail.com>
2026-08-03 04:28:23 +0100

    step 4 cpp

--- src/engine/Board.cpp
+++ src/engine/Board.cpp
@@ -58,6 +58,40 @@ bool Board::checkWin(Move last, Player p) const {
     return false;
 }
 
+int Board::checkAndApplyCaptures(Move last, Player p) {
+    static const int dirs8[8][2] = {
+        {1,0}, {-1,0}, {0,1}, {0,-1},
+        {1,1}, {-1,-1}, {1,-1}, {-1,1}
+    };
+
+    Cell mine = (p == Player::Black) ? Cell::Black : Cell::White;
+    Cell opp  = (p == Player::Black) ? Cell::White : Cell::Black;
+    int capturedCount = 0;
+
+    for (const auto& d : dirs8) {
+        int dx = d[0], dy = d[1];
+        int x1 = last.x + dx,     y1 = last.y + dy;
+        int x2 = last.x + 2 * dx, y2 = last.y + 2 * dy;
+        int x3 = last.x + 3 * dx, y3 = last.y + 3 * dy;
+
+        if (isInBounds(x1, y1) && isInBounds(x2, y2) && isInBounds(x3, y3)
+            && grid[y1][x1] == opp && grid[y2][x2] == opp && grid[y3][x3] == mine) {
+            grid[y1][x1] = Cell::Empty;
+            grid[y2][x2] = Cell::Empty;
+            capturedCount += 2;
+        }
+    }
+
+    if (p == Player::Black) capturedByBlack += capturedCount;
+    else capturedByWhite += capturedCount;
+
+    return capturedCount;
+}
+
+int Board::capturedBy(Player p) const {
+    return (p == Player::Black) ? capturedByBlack : capturedByWhite;
+}
+
 void Board::print() const {
     for (int y = 0; y < SIZE; y++) {
         for (int x = 0; x < SIZE; x++) {
